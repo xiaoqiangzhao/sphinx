ccf23d486af865f6d72ce79c87228a756de63293
Qiang Zhao-B51816 <b51816@lvd5754.ap.freescale.net>
2017-04-09 12:45:17 +0530

    almost done

--- jay_doc.py
+++ jay_doc.py
@@ -1,10 +1,11 @@
 
 SEP = ['#', '*', '=', '-', '^', "\""]
 PLACEHOLD = "<ADD DESCRIPTION HERE!>"
+TYPE = ['doc','para','list','tag_s','tag_e','code','url','download','include_f']
 
 class para(object):
-    def __init__(self, title, articles = None, depth = 0):
-        self.title = title
+    def __init__(self, name, articles = None, depth = 0):
+        self.name = name
         self.depth = depth
         if articles :
             self.articles = articles
@@ -15,22 +16,32 @@ class para(object):
         self.type = 'para'
 
     def add_article(self, item):
-        if isinstance(item, para):
-            item.depth = self.depth + 1
+        item.depth = self.depth + 1
         self.articles.append(item)
+
+    def print_topo(self):
+        print("  "*self.depth, self.type, ":",  self.name)
+        for a in self.articles:
+            a.print_topo()
 
     def show_articles(self):
         for i in self.articles:
             if isinstance(i,misc_item):
                 print(i.entity)
             else:
-                print(i.title)
+                print(i.name)
 
 
 class misc_item:
-    def __init__(self, entity= None, type_i = ''):
+    def __init__(self, name , entity= None, type_i = '', depth = 0):
         self.entity = entity
         self.type = type_i
+        self.depth = depth
+        self.name = name
+
+    def print_topo(self):
+        print("  "*self.depth, self.type, ":", self.name)
+
 
 
 class doc:
@@ -44,6 +55,7 @@ class doc:
         self.depth = 0
         self.sep = SEP
         self.des = PLACEHOLD
+        self.type = 'doc'
 
     def add_para(self, para):
         '''
@@ -51,3 +63,8 @@ class doc:
         '''
         para.depth = self.depth + 1
         self.paras.append(para)
+
+    def print_topo(self):
+        print(self.topic)
+        for p in self.paras:
+            p.print_topo()
--- test.py
+++ test.py
@@ -9,22 +9,49 @@ doc1 = doc("my test","Jay Zhao")
 p1 = para("para1")
 p2 = para("para2")
 p3 = para("para3")
-doc1.add_para(p3)
 p4 = para("para4")
+doc1.add_para(p1)
+doc1.add_para(p2)
+doc1.add_para(p3)
 p3.add_article(p4)
 
 l3 = ['a','b','c']
-i3 = misc_item(l3,'list')
+ts1 = {'name':'touch me'}
+i3 = misc_item('list_for_p3', l3, 'list')
+tag_s1 = misc_item('ts1',ts1,'tag_s')
+p1.add_article(tag_s1)
 # p3 = para("para3",[p4,i3])
 p3.add_article(i3)
+te1 = {'name':'touch me','title':'tag'}
+tag_e1 = misc_item('te1',te1,'tag_e')
+p4.add_article(tag_e1)
+cb_t = []
+with open('./test.py','r') as f:
+    cb_t = f.readlines()
 
-doc1.add_para(p1)
-doc1.add_para(p2)
+cb = {'name':'example code', 'text': cb_t, 'shell': False}
 
-p1.show_articles()
-p2.show_articles()
-p3.show_articles()
-p4.show_articles()
+code4 = misc_item('code4',cb,'code')
+
+inc_en = {'name':'jay_doc','file_path':'../jay_doc.py'}
+inc3 = misc_item('inc3',inc_en,'include_f')
+p2.add_article(inc3)
+
+url1 = misc_item('url',None,'url')
+url1.entity = {'name':'Ref Code','desc':'As with more details, please see','url':'https://www.baidu.com'}
+p1.add_article(url1)
+dl = misc_item('download',None,'download')
+dl.entity = {'name':'Click Me','desc':'Or You can download the Source File','file_path':'source/jay_doc.py'}
+p1.add_article(dl)
+p4.add_article(code4)
+
+
+
+doc1.print_topo()
+# p1.show_articles()
+# p2.show_articles()
+# p3.show_articles()
+# p4.show_articles()
 
 
 with open('./test/test.rst','w') as f:
